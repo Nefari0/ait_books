f7733bdcd398bf15b976fdba25801864c414e7e6
Nefari0 <chrisdemontigny@yahoo.com>
2026-08-08 11:14:05 -0600

    Adding typescript

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { stores } from './stores';
+import { AppContainer, Library, BookImage } from './app.styles';
+import { StoreItem } from './Components/StoreItems/storeItem.component';
+import Header from './Components/Header/header.component';
+import bookandtablet from './3d book and tablet.png';
+
+function App() {
+  const storeMap = stores.map((el, i) => {
+    return <StoreItem key={i} el={el} />;
+  });
+
+  return (
+    <AppContainer>
+      <Header />
+      <BookImage src={bookandtablet} />
+      <Library>
+        {storeMap}
+      </Library>
+    </AppContainer>
+  );
+}
+
+export default App;
