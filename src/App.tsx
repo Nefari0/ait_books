5b3c07c4b42577ed839b240fd6d907b7415bdaf0
Nefari0 <chrisdemontigny@yahoo.com>
2026-08-08 12:09:48 -0600

    Typescript working

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { stores } from './stores';
 import { AppContainer, Library, BookImage } from './app.styles';
-import { StoreItem } from './Components/StoreItems/storeItem.component.tsx';
+// import { StoreItem } from './Components/StoreItems/storeItem.component';
+import { StoreItem } from './Components/StoreItems/storeItem.component';
 import Header from './Components/Header/header.component';
 import bookandtablet from './3d book and tablet.png';
 
