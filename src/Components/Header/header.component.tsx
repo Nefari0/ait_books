af4d53e30e5a55dac5b6403a5b5cd12312e511e1
Nefari0 <chrisdemontigny@yahoo.com>
2026-08-17 20:27:37 -0600

    Added image

--- src/Components/Header/header.component.tsx
+++ src/Components/Header/header.component.tsx
@@ -1,13 +1,17 @@
-import { HeaderContainer,AuthorLink } from "./header.styles";
+import { HeaderContainer,AuthorLink,SocialMediaIcons,AuthorImage } from "./header.styles";
 
 function Header() {
   return (
     <HeaderContainer>
+            
         <AuthorLink 
             href="https://chris.madmodels3d.com/"
             target="_blank"
             rel="noopener noreferrer"
         >
+        <AuthorImage>
+           <img src={"https://firebasestorage.googleapis.com/v0/b/bcwb-879fa.appspot.com/o/data%2Fimages%2Fc_demontigny?alt=media&token=782d914e-3cda-4402-bd68-aa30ed57b6bd"} />
+        </AuthorImage> 
             By Chris deMontigny
         </AuthorLink>
 
@@ -17,7 +21,7 @@ function Header() {
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                <img src="https://cdn-icons-png.flaticon.com/128/174/174855.png" />
+                <SocialMediaIcons src="https://cdn-icons-png.flaticon.com/128/174/174855.png" />
             </a>
 
             <a 
@@ -25,7 +29,7 @@ function Header() {
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                <img src="https://cdn-icons-png.flaticon.com/512/13051/13051733.png"/>
+                <SocialMediaIcons src="https://cdn-icons-png.flaticon.com/512/13051/13051733.png"/>
             </a>
         </div>
     </HeaderContainer>
--- src/Components/Header/header.styles.ts
+++ src/Components/Header/header.styles.ts
@@ -17,12 +17,38 @@ export const HeaderContainer = styled.header`
         text-decoration:none;
         margin:5px;
     }
+`
+
+export const SocialMediaIcons = styled.img`
+    width:50px;
+
+    @media (${query1}) {
+        width:25px;
+    }
+`
+
+export const AuthorImage = styled.div`
+    width:50px;
+    height:50px;
+    // height:auto;
+    border-radius:50%;
+    background-color:blue;
+    overflow:hidden;
+    position:relative;
+    margin:10px;
+    
+    @media (max-width:304px) {
+        width:35px;
+        height:35px;
+    }
 
     img {
-        width:50px;
+        height:100%;
+        left:-10px;
+        position:absolute;
 
-        @media (${query1}) {
-            width:25px;
+        @media (max-width:304px) {
+            left:-5px;
         }
     }
 `
@@ -35,4 +61,8 @@ export const AuthorLink = styled.a`
     color: #fff;
     text-decoration: none;
     transition: color 0.2s ease;
+
+    display:flex;
+    align-items: center;
+
 `
